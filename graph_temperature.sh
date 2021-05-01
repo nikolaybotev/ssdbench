b055e6ea308a6566409cab71f58abd1d8b6d9748
Nikolay Botev <nikolaybotev.b@gmail.com>
2021-05-01 18:06:37 -0400

    Extract gnuplot code into its own file

--- graph_temperature.sh
+++ graph_temperature.sh
@@ -1,9 +1,1 @@
-gnuplot -e "
-set terminal svg;
-set title '${DISK_NAME}';
-set xlabel 'Time (seconds)';
-set ylabel 'Temperature (C)';
-set yrange [15:85];
-set style data line;
-plot for [i=2:3] '${DISK_TEMPERATURE_FILE}' using i title columnheader linewidth 2
-" > "${DISK_TEMPERATURE_GRAPH}"
+gnuplot -c graph_temperature.gpi "${DISK_NAME}" "${DISK_TEMPERATURE_FILE}" > "${DISK_TEMPERATURE_GRAPH}"
