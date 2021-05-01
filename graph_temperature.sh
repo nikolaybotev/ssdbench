9cb2d816990e5b7444bfbd08a029de6d5887ffd8
Nikolay Botev <nikolaybotev.b@gmail.com>
2021-05-01 18:53:43 -0400

    Set SVG title

--- graph_temperature.sh
+++ graph_temperature.sh
@@ -1,1 +1,2 @@
 gnuplot -c tools/graph_temperature.gpi "${DISK_NAME}" "${DISK_TEMPERATURE_FILE}" > "${DISK_TEMPERATURE_GRAPH}"
+sed -i "" -e "s|<title>Gnuplot</title>|<title>${DISK_NAME}</title>|" "${DISK_TEMPERATURE_GRAPH}"
