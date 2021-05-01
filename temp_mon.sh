07b18b1fe6c660405726303105094215ec5b1551
Nikolay Botev <nikolaybotev.b@gmail.com>
2021-05-01 16:52:59 -0400

    Support second temperature sensor of Samsung 980 Pro

--- temp_mon.sh
+++ temp_mon.sh
@@ -1,6 +1,10 @@
 n=1
 while true; do
-  smartctl -A "$DISK_ID" | awk "/Temperature/ { print \"$n\\t\" \$2 }"
+  output="$(smartctl -A "$DISK_ID")"
+  main_temp="$(echo "$output" | awk "/^Temperature:/ { print \$2 }")"
+  # Support for second temperature sensor
+  second_temp="$(echo "$output" | awk "/^Temperature Sensor 2:/ { print \$4 }")"
+  echo "$n\t$main_temp\t$second_temp"
   sleep 1
   n=$((n+1))
 done | tee "${DISK_TEMPERATURE_FILE}"
